e212a2e73a8582c550094cb740300c60cfcd0e10
Mario Henrique Martins da Silva <mario.henrique10@outlook.com.br>
2024-12-04 12:00:55 -0300

    Salvando novos arquivos

--- Sprint04/desafio/mascarar-dados/mascarar-dados.py
+++ Sprint04/desafio/mascarar-dados/mascarar-dados.py
@@ -2,5 +2,5 @@ import hashlib #Importação da biblioteca
 
 while True: #Colocar o script sempre em execução
     string=input("Digite: ") #Ler uma entrada e armazenar em uma variável
-    hashed_string=hashlib.sha256(string.encode('utf-8')).hexdigest() #Atribuir uma hash para a string de entrada por meio do algoritmo SHA-1
+    hashed_string=hashlib.sha1(string.encode('utf-8')).hexdigest() #Atribuir uma hash para a string de entrada por meio do algoritmo SHA-1
     print("Hash:", hashed_string) #Imprimir a hash para a string
